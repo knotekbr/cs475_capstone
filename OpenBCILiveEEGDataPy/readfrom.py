7671f144fed94af94558436c18f8a02b0c7c8141
Jack <wjack9947@gmail.com>
2022-02-20 12:30:19 -0500

    Addition of Week3 Report and LiveEEG Data stream code

---
+++ OpenBCILiveEEGDataPy/readfrom.py
@@ -0,0 +1,27 @@
+#SOURCE OpenBCI.com
+#used to produce brain wave frequency graphs
+
+from pyOpenBCI import OpenBCICyton
+import time
+from collections import deque
+import numpy as np
+import cv2
+
+last_print=time.time()
+fps_counter=deque(maxLen=50)
+sequence=np.zeros((100,16))
+counter=0
+
+def print_raw(sample):
+    global last_print
+    global sequence
+    global counter
+    sequence=np.roll(sequence,1,0)
+    sequence[0, ...]=sample.channels_data
+
+    fps_counter.append(time.time()-last_print)
+    last_print=time.time()
+    print(f'FPS: {1/(sum(fps_counter)/len(fps_counter)):.2f}, : {len(sequence)}, ... {counter}')
+
+    board.OpenBCICyton(daisy=True)
+    board.start_stream(print_raw)
